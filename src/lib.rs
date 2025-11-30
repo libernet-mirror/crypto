d0f97bb0fb9136bbd277bd1cc6308683fa30fa9c
71104 <a71104@gmail.com>
2025-11-30 11:15:33 +0100

    Revert "Refactor wallet seeds."
    This reverts commit 1bf1a79388298593dcc50361c53af89783addd7f.

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@
 
 use crate::merkle::AsScalar;
 use crate::signer::{BlsVerifier, EcDsaVerifier, Ed25519Verifier, Signer};
+use anyhow::Context;
 use blstrs::{G1Affine, Scalar};
 use primitive_types::H512;
 use std::time::{Duration, UNIX_EPOCH};
@@ -347,7 +348,7 @@ impl Wallet {
     pub fn load(seed: &str, commitment: &str, y: Vec<String>) -> Result<Self, JsValue> {
         Ok(Self {
             inner: wallet::Wallet::load(
-                utils::parse_scalar(seed).map_err(map_err)?,
+                seed.parse().context("invalid seed").map_err(map_err)?,
                 utils::parse_g1(commitment).map_err(map_err)?,
                 &y.iter()
                     .map(|y| utils::parse_g1(y.as_str()).map_err(map_err))
@@ -360,7 +361,7 @@ impl Wallet {
 
     #[wasm_bindgen]
     pub fn seed(&self) -> String {
-        utils::format_scalar(self.inner.seed())
+        format!("{:#x}", self.inner.seed())
     }
 
     #[wasm_bindgen]
--- src/wallet.rs
+++ src/wallet.rs
@@ -19,26 +19,32 @@ fn get_argon2_params() -> argon2::Params {
     argon2::Params::new(1024, 2, 1, Some(64)).unwrap()
 }
 
-pub fn derive_key(password: &str, seed: Scalar) -> Scalar {
+pub fn derive_key(password: &str, seed: H512) -> Scalar {
     let mut bytes = [0u8; 64];
     Argon2::new(
         argon2::Algorithm::Argon2id,
         argon2::Version::V0x13,
         get_argon2_params(),
     )
-    .hash_password_into(password.as_bytes(), &seed.to_bytes_le(), &mut bytes)
+    .hash_password_into(password.as_bytes(), seed.as_fixed_bytes(), &mut bytes)
     .unwrap();
     utils::h512_to_scalar(H512::from_slice(&bytes))
 }
 
 #[derive(Debug)]
 pub struct Wallet {
-    seed: Scalar,
+    seed: H512,
     commitment: G1Affine,
     proofs: [Proof; MAX_PASSWORDS],
 }
 
 impl Wallet {
+    fn get_random_seed() -> H512 {
+        let mut bytes = [0u8; 64];
+        getrandom::getrandom(&mut bytes).unwrap();
+        H512::from_slice(&bytes)
+    }
+
     fn shuffle_proofs(proofs: &mut Vec<Proof>) {
         for i in 0..proofs.len() {
             let mut bytes = [0u8; 64];
@@ -60,7 +66,7 @@ impl Wallet {
                 MAX_PASSWORDS
             ));
         }
-        let seed = utils::get_random_scalar();
+        let seed = Self::get_random_seed();
         let mut keys: Vec<Scalar> = passwords
             .iter()
             .map(|password| derive_key(password.as_str(), seed))
@@ -90,7 +96,7 @@ impl Wallet {
         })
     }
 
-    pub fn load(seed: Scalar, commitment: G1Affine, y: &[G1Affine; MAX_PASSWORDS]) -> Self {
+    pub fn load(seed: H512, commitment: G1Affine, y: &[G1Affine; MAX_PASSWORDS]) -> Self {
         Self {
             seed,
             commitment,
@@ -98,7 +104,7 @@ impl Wallet {
         }
     }
 
-    pub fn seed(&self) -> Scalar {
+    pub fn seed(&self) -> H512 {
         self.seed
     }
 
@@ -127,7 +133,7 @@ impl Wallet {
         for proof in &self.proofs {
             if proof.verify(self.commitment, key, 0.into()).is_ok() {
                 let mut hasher = sha3::Sha3_512::new();
-                hasher.update(self.seed.to_bytes_le());
+                hasher.update(self.seed.as_fixed_bytes());
                 hasher.update(key.to_bytes_le());
                 hasher.update((index as u64).to_le_bytes());
                 let secret_key = H512::from_slice(&hasher.finalize());
@@ -144,24 +150,23 @@ mod tests {
     use crate::signer::BlsVerifier;
     use utils::testing::parse_scalar;
 
-    fn seed() -> Scalar {
-        Scalar::from_bytes_le(&[
+    fn seed() -> H512 {
+        H512::from_slice(&[
             1u8, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
-            24, 25, 26, 27, 28, 29, 30, 31, 0,
+            24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45,
+            46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64,
         ])
-        .into_option()
-        .unwrap()
     }
 
     #[test]
     fn test_derive_key() {
         assert_eq!(
             derive_key("lorem ipsum dolor sit amet", seed()),
-            parse_scalar("0x27b1650a4657cf54f313c2de70ea300a235c545a08f2df0f15af1b8e57e039d3")
+            parse_scalar("0x274029075d5704edc31f852fa778b3f08d4fe23384d2af341eb30415a682e2bc")
         );
         assert_eq!(
             derive_key("sator arepo tenet opera rotas", seed()),
-            parse_scalar("0x66efe6905b8dbe8bc84a4c820f8c4d0cba2b8b89c639042e9adf2a3a85df2e48")
+            parse_scalar("0x673c739883a03d8d213349fe958a774d733bd9145dee11f5c760d88d9756a5cd")
         );
     }
 
