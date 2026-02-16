49ecff56a2de35d0465049fa6022fe0651d01386
71104 <a71104@gmail.com>
2026-02-16 04:37:48 +0100

    Add the bit comparator chip.

--- src/xits.rs
+++ src/xits.rs
@@ -33,9 +33,9 @@ pub fn decompose_scalar_bits<const N: usize>(value: Scalar) -> [Scalar; N] {
 }
 
 #[derive(Debug, Default)]
-pub struct BitDecompositionChip<const N: usize> {}
+pub struct BitDecomposerChip<const N: usize> {}
 
-impl<const N: usize> plonk::Chip<1, N> for BitDecompositionChip<N> {
+impl<const N: usize> plonk::Chip<1, N> for BitDecomposerChip<N> {
     fn build(
         &self,
         builder: &mut plonk::CircuitBuilder,
@@ -47,7 +47,7 @@ impl<const N: usize> plonk::Chip<1, N> for BitDecompositionChip<N> {
             sum = builder.add_linear_combination_gate(1.into(), sum.into(), power, None);
             power = power.double();
             let bit = Some(plonk::Wire::RightIn(sum.gate()));
-            builder.add_bool_assertion_gate(bit);
+            builder.add_bit_assertion_gate(bit);
             bit
         });
         if let Some(input) = inputs[0] {
@@ -72,11 +72,85 @@ impl<const N: usize> plonk::Chip<1, N> for BitDecompositionChip<N> {
             input = shr1(input);
             sum = witness.combine(1.into(), sum.into(), power, bit.into());
             power = power.double();
-            let bit = plonk::Wire::RightIn(sum.gate());
-            witness.assert_bool(bit);
-            bit.into()
+            let bit = plonk::Wire::RightIn(sum.gate()).into();
+            witness.assert_bit(bit);
+            bit
         });
         Ok(bits)
+    }
+}
+
+#[derive(Debug)]
+pub struct BitComparatorChip<const N: usize> {
+    rhs: U256,
+}
+
+impl<const N: usize> BitComparatorChip<N> {
+    pub fn new(rhs: U256) -> Self {
+        Self { rhs }
+    }
+}
+
+impl<const N: usize> BitComparatorChip<N> {
+    fn get_rhs_bit(&self, i: usize) -> Scalar {
+        utils::u256_to_scalar((self.rhs >> i) & 1.into()).unwrap()
+    }
+
+    fn add_logical_not_gate(
+        builder: &mut plonk::CircuitBuilder,
+        input: plonk::Wire,
+    ) -> plonk::Wire {
+        builder.add_unary_gate(
+            0.into(),
+            0.into(),
+            -Scalar::from(1),
+            -Scalar::from(1),
+            1.into(),
+            input.into(),
+        )
+    }
+
+    fn logical_not(witness: &mut plonk::Witness, input: plonk::Wire) -> plonk::Wire {
+        let gate = witness.pop_gate();
+        witness.copy(input.into(), plonk::Wire::LeftIn(gate));
+        let input = witness.copy(input.into(), plonk::Wire::RightIn(gate));
+        let out = plonk::Wire::Out(gate);
+        witness.set(out, Scalar::from(1) - input.square());
+        out
+    }
+}
+
+impl<const N: usize> plonk::Chip<N, 1> for BitComparatorChip<N> {
+    fn build(
+        &self,
+        builder: &mut plonk::CircuitBuilder,
+        inputs: [Option<plonk::Wire>; N],
+    ) -> Result<[Option<plonk::Wire>; 1]> {
+        assert!(N > 0);
+        let mut cmp = builder.add_sub_const_gate(inputs[N - 1], self.get_rhs_bit(N - 1));
+        for i in (0..(N - 1)).rev() {
+            let cmp2 = builder.add_sub_const_gate(inputs[i], self.get_rhs_bit(i));
+            let not = Self::add_logical_not_gate(builder, cmp);
+            let rhs = builder.add_mul_gate(cmp2.into(), not.into());
+            cmp = builder.add_sum_gate(cmp.into(), rhs.into());
+        }
+        Ok([Some(cmp)])
+    }
+
+    fn witness(
+        &self,
+        witness: &mut plonk::Witness,
+        inputs: [plonk::WireOrUnconstrained; N],
+    ) -> Result<[plonk::WireOrUnconstrained; 1]> {
+        assert!(N > 0);
+        let mut cmp = witness.sub_const(inputs[N - 1], self.get_rhs_bit(N - 1));
+        for i in (0..(N - 1)).rev() {
+            let cmp2 = witness.sub_const(inputs[i], self.get_rhs_bit(i));
+            let not = Self::logical_not(witness, cmp);
+            let rhs = witness.mul(cmp2.into(), not.into());
+            cmp = witness.add(cmp.into(), rhs.into());
+        }
+        Ok([cmp.into()])
     }
 }
 
@@ -111,31 +185,9 @@ pub fn decompose_scalar_trits<const N: usize>(value: Scalar) -> [Scalar; N] {
 }
 
 #[derive(Debug, Default)]
-pub struct TritDecompositionChip<const N: usize> {}
+pub struct TritDecomposerChip<const N: usize> {}
 
-impl<const N: usize> TritDecompositionChip<N> {
-    fn add_trit_assertion_gates(builder: &mut plonk::CircuitBuilder, trit: Option<plonk::Wire>) {
-        let lhs = builder.add_poly2_gate(1.into(), -Scalar::from(3), 2.into(), trit);
-        builder.add_binary_gate(
-            0.into(),
-            0.into(),
-            0.into(),
-            1.into(),
-            0.into(),
-            lhs.into(),
-            trit,
-        );
-    }
-
-    fn assert_trit(witness: &mut plonk::Witness, trit: plonk::Wire) {
-        let lhs = witness.poly2(1.into(), -Scalar::from(3), 2.into(), trit.into());
-        let gate = witness.pop_gate();
-        witness.copy(lhs.into(), plonk::Wire::LeftIn(gate));
-        witness.copy(trit.into(), plonk::Wire::RightIn(gate));
-    }
-}
-
-impl<const N: usize> plonk::Chip<1, N> for TritDecompositionChip<N> {
+impl<const N: usize> plonk::Chip<1, N> for TritDecomposerChip<N> {
     fn build(
         &self,
         builder: &mut plonk::CircuitBuilder,
@@ -147,7 +199,7 @@ impl<const N: usize> plonk::Chip<1, N> for TritDecompositionChip<N> {
             sum = builder.add_linear_combination_gate(1.into(), sum.into(), power, None);
             power = power.double() + power;
             let trit = Some(plonk::Wire::RightIn(sum.gate()));
-            Self::add_trit_assertion_gates(builder, trit);
+            builder.add_trit_assertion_gate(trit);
             trit
         });
         if let Some(input) = inputs[0] {
@@ -172,9 +224,9 @@ impl<const N: usize> plonk::Chip<1, N> for TritDecompositionChip<N> {
             input = div3(input);
             sum = witness.combine(1.into(), sum.into(), power, trit.into());
             power = power.double() + power;
-            let trit = plonk::Wire::RightIn(sum.gate());
-            Self::assert_trit(witness, trit);
-            trit.into()
+            let trit = plonk::Wire::RightIn(sum.gate()).into();
+            witness.assert_trit(trit);
+            trit
         });
         Ok(trits)
     }
@@ -184,6 +236,8 @@ impl<const N: usize> plonk::Chip<1, N> for TritDecompositionChip<N> {
 mod tests {
     use super::*;
     use crate::plonk::{Chip, WireOrUnconstrained};
+    use std::cmp::Ordering;
+    use std::collections::BTreeMap;
     use utils::testing::parse_scalar;
 
     #[test]
@@ -337,10 +391,10 @@ mod tests {
         );
     }
 
-    fn test_bit_decomposition_chip<const N: usize>(value: u64) {
+    fn test_bit_decomposer_chip<const N: usize>(value: u64) {
         let mut builder = plonk::CircuitBuilder::default();
         let input = builder.add_const_gate(value.into());
-        let chip = BitDecompositionChip::<N>::default();
+        let chip = BitDecomposerChip::<N>::default();
         assert!(chip.build(&mut builder, [Some(input)]).is_ok());
         let mut witness = plonk::Witness::new(builder.len());
         let input = witness.assert_constant(value.into());
@@ -359,29 +413,90 @@ mod tests {
     }
 
     #[test]
-    fn test_bit_decomposition_chip_1() {
-        test_bit_decomposition_chip::<1>(0);
-        test_bit_decomposition_chip::<1>(1);
+    fn test_bit_decomposer_chip_1() {
+        test_bit_decomposer_chip::<1>(0);
+        test_bit_decomposer_chip::<1>(1);
     }
 
     #[test]
-    fn test_bit_decomposition_chip_2() {
-        test_bit_decomposition_chip::<2>(0);
-        test_bit_decomposition_chip::<2>(1);
-        test_bit_decomposition_chip::<2>(2);
-        test_bit_decomposition_chip::<2>(3);
+    fn test_bit_decomposer_chip_2() {
+        test_bit_decomposer_chip::<2>(0);
+        test_bit_decomposer_chip::<2>(1);
+        test_bit_decomposer_chip::<2>(2);
+        test_bit_decomposer_chip::<2>(3);
     }
 
     #[test]
-    fn test_bit_decomposition_chip_3() {
-        test_bit_decomposition_chip::<3>(0);
-        test_bit_decomposition_chip::<3>(1);
-        test_bit_decomposition_chip::<3>(2);
-        test_bit_decomposition_chip::<3>(3);
-        test_bit_decomposition_chip::<3>(4);
-        test_bit_decomposition_chip::<3>(5);
-        test_bit_decomposition_chip::<3>(6);
-        test_bit_decomposition_chip::<3>(7);
+    fn test_bit_decomposer_chip_3() {
+        test_bit_decomposer_chip::<3>(0);
+        test_bit_decomposer_chip::<3>(1);
+        test_bit_decomposer_chip::<3>(2);
+        test_bit_decomposer_chip::<3>(3);
+        test_bit_decomposer_chip::<3>(4);
+        test_bit_decomposer_chip::<3>(5);
+        test_bit_decomposer_chip::<3>(6);
+        test_bit_decomposer_chip::<3>(7);
+    }
+
+    fn test_bit_comparator_chip<const N: usize>(lhs: u64, rhs: u64) {
+        let mut builder = plonk::CircuitBuilder::default();
+        let input = builder.add_const_gate(lhs.into());
+        let decomposer_chip = BitDecomposerChip::<N>::default();
+        let bits = decomposer_chip.build(&mut builder, [input.into()]).unwrap();
+        let comparator_chip = BitComparatorChip::<N>::new(rhs.into());
+        let cmp = comparator_chip.build(&mut builder, bits).unwrap()[0].unwrap();
+        builder.declare_public_inputs([input, cmp]);
+        let mut witness = plonk::Witness::new(builder.len());
+        let input = witness.assert_constant(lhs.into());
+        let bits = decomposer_chip
+            .witness(&mut witness, [input.into()])
+            .unwrap();
+        assert!(comparator_chip.witness(&mut witness, bits).is_ok());
+        assert!(builder.check_witness(&witness).is_ok());
+        let circuit = builder.build();
+        let proof = circuit.prove(witness).unwrap();
+        assert_eq!(
+            circuit.verify(&proof).unwrap(),
+            BTreeMap::from([
+                (input, lhs.into()),
+                (
+                    cmp,
+                    match lhs.cmp(&rhs) {
+                        Ordering::Less => -Scalar::from(1),
+                        Ordering::Equal => 0.into(),
+                        Ordering::Greater => 1.into(),
+                    }
+                )
+            ])
+        );
+    }
+
+    #[test]
+    fn test_bit_comparator_chip_1() {
+        test_bit_comparator_chip::<1>(0, 0);
+        test_bit_comparator_chip::<1>(1, 0);
+        test_bit_comparator_chip::<1>(0, 1);
+        test_bit_comparator_chip::<1>(1, 1);
+    }
+
+    #[test]
+    fn test_bit_comparator_chip_2() {
+        test_bit_comparator_chip::<2>(0, 0);
+        test_bit_comparator_chip::<2>(1, 0);
+        test_bit_comparator_chip::<2>(2, 0);
+        test_bit_comparator_chip::<2>(3, 0);
+        test_bit_comparator_chip::<2>(0, 1);
+        test_bit_comparator_chip::<2>(1, 1);
+        test_bit_comparator_chip::<2>(2, 1);
+        test_bit_comparator_chip::<2>(3, 1);
+        test_bit_comparator_chip::<2>(0, 2);
+        test_bit_comparator_chip::<2>(1, 2);
+        test_bit_comparator_chip::<2>(2, 2);
+        test_bit_comparator_chip::<2>(3, 2);
+        test_bit_comparator_chip::<2>(0, 3);
+        test_bit_comparator_chip::<2>(1, 3);
+        test_bit_comparator_chip::<2>(2, 3);
+        test_bit_comparator_chip::<2>(3, 3);
     }
 
     #[test]
@@ -544,10 +659,10 @@ mod tests {
         );
     }
 
-    fn test_trit_decomposition_chip<const N: usize>(value: u64) {
+    fn test_trit_decomposer_chip<const N: usize>(value: u64) {
         let mut builder = plonk::CircuitBuilder::default();
         let input = builder.add_const_gate(value.into());
-        let chip = TritDecompositionChip::<N>::default();
+        let chip = TritDecomposerChip::<N>::default();
         assert!(chip.build(&mut builder, [Some(input)]).is_ok());
         let mut witness = plonk::Witness::new(builder.len());
         let input = witness.assert_constant(value.into());
@@ -566,53 +681,53 @@ mod tests {
     }
 
     #[test]
-    fn test_trit_decomposition_chip_1() {
-        test_trit_decomposition_chip::<1>(0);
-        test_trit_decomposition_chip::<1>(1);
-        test_trit_decomposition_chip::<1>(2);
+    fn test_trit_decomposer_chip_1() {
+        test_trit_decomposer_chip::<1>(0);
+        test_trit_decomposer_chip::<1>(1);
+        test_trit_decomposer_chip::<1>(2);
     }
 
     #[test]
-    fn test_trit_decomposition_chip_2() {
-        test_trit_decomposition_chip::<2>(0);
-        test_trit_decomposition_chip::<2>(1);
-        test_trit_decomposition_chip::<2>(2);
-        test_trit_decomposition_chip::<2>(3);
-        test_trit_decomposition_chip::<2>(4);
-        test_trit_decomposition_chip::<2>(5);
-        test_trit_decomposition_chip::<2>(6);
-        test_trit_decomposition_chip::<2>(7);
-        test_trit_decomposition_chip::<2>(8);
+    fn test_trit_decomposer_chip_2() {
+        test_trit_decomposer_chip::<2>(0);
+        test_trit_decomposer_chip::<2>(1);
+        test_trit_decomposer_chip::<2>(2);
+        test_trit_decomposer_chip::<2>(3);
+        test_trit_decomposer_chip::<2>(4);
+        test_trit_decomposer_chip::<2>(5);
+        test_trit_decomposer_chip::<2>(6);
+        test_trit_decomposer_chip::<2>(7);
+        test_trit_decomposer_chip::<2>(8);
     }
 
     #[test]
-    fn test_trit_decomposition_chip_3() {
-        test_trit_decomposition_chip::<3>(0);
-        test_trit_decomposition_chip::<3>(1);
-        test_trit_decomposition_chip::<3>(2);
-        test_trit_decomposition_chip::<3>(3);
-        test_trit_decomposition_chip::<3>(4);
-        test_trit_decomposition_chip::<3>(5);
-        test_trit_decomposition_chip::<3>(6);
-        test_trit_decomposition_chip::<3>(7);
-        test_trit_decomposition_chip::<3>(8);
-        test_trit_decomposition_chip::<3>(9);
-        test_trit_decomposition_chip::<3>(10);
-        test_trit_decomposition_chip::<3>(11);
-        test_trit_decomposition_chip::<3>(12);
-        test_trit_decomposition_chip::<3>(13);
-        test_trit_decomposition_chip::<3>(14);
-        test_trit_decomposition_chip::<3>(15);
-        test_trit_decomposition_chip::<3>(16);
-        test_trit_decomposition_chip::<3>(17);
-        test_trit_decomposition_chip::<3>(18);
-        test_trit_decomposition_chip::<3>(19);
-        test_trit_decomposition_chip::<3>(20);
-        test_trit_decomposition_chip::<3>(21);
-        test_trit_decomposition_chip::<3>(22);
-        test_trit_decomposition_chip::<3>(23);
-        test_trit_decomposition_chip::<3>(24);
-        test_trit_decomposition_chip::<3>(25);
-        test_trit_decomposition_chip::<3>(26);
+    fn test_trit_decomposer_chip_3() {
+        test_trit_decomposer_chip::<3>(0);
+        test_trit_decomposer_chip::<3>(1);
+        test_trit_decomposer_chip::<3>(2);
+        test_trit_decomposer_chip::<3>(3);
+        test_trit_decomposer_chip::<3>(4);
+        test_trit_decomposer_chip::<3>(5);
+        test_trit_decomposer_chip::<3>(6);
+        test_trit_decomposer_chip::<3>(7);
+        test_trit_decomposer_chip::<3>(8);
+        test_trit_decomposer_chip::<3>(9);
+        test_trit_decomposer_chip::<3>(10);
+        test_trit_decomposer_chip::<3>(11);
+        test_trit_decomposer_chip::<3>(12);
+        test_trit_decomposer_chip::<3>(13);
+        test_trit_decomposer_chip::<3>(14);
+        test_trit_decomposer_chip::<3>(15);
+        test_trit_decomposer_chip::<3>(16);
+        test_trit_decomposer_chip::<3>(17);
+        test_trit_decomposer_chip::<3>(18);
+        test_trit_decomposer_chip::<3>(19);
+        test_trit_decomposer_chip::<3>(20);
+        test_trit_decomposer_chip::<3>(21);
+        test_trit_decomposer_chip::<3>(22);
+        test_trit_decomposer_chip::<3>(23);
+        test_trit_decomposer_chip::<3>(24);
+        test_trit_decomposer_chip::<3>(25);
+        test_trit_decomposer_chip::<3>(26);
     }
 }
